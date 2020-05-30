7cc447dcbea5f96d9aa37cab03493acb34ad7007
mitsutoshi <mitsu3204@gmail.com>
2020-05-30 18:43:10 +0900

    Fixed test code

--- helper_test.go
+++ helper_test.go
@@ -1,7 +1,6 @@
 package bitflyergo
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
@@ -10,9 +9,6 @@ func TestCreateOHLC1Sec(t *testing.T) {
 	candles, err := CreateOHLC(getExecutions(), 1)
 	if err != nil {
 		t.Fatal(err)
-	}
-	for i, c := range candles {
-		fmt.Printf("%v, %v\n", i, c)
 	}
 	if len(candles) != 7 {
 		t.Fatalf("Expect: 7, Actual: %v, %s", len(candles), "length of candles is not match.")
@@ -29,9 +25,6 @@ func TestCreateOHLC2Sec(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	for i, c := range candles {
-		fmt.Printf("%v, %v\n", i, c)
-	}
 	if len(candles) != 5 {
 		t.Fatalf("Expect: 7, Actual: %v, %s", len(candles), "length of candles is not match.")
 	}
@@ -41,9 +34,6 @@ func TestCreateOHLC5Sec(t *testing.T) {
 	candles, err := CreateOHLC(getExecutions(), 5)
 	if err != nil {
 		t.Fatal(err)
-	}
-	for i, c := range candles {
-		fmt.Printf("%v, %v\n", i, c)
 	}
 	if len(candles) != 3 {
 		t.Fatalf("Expect: 2, Actual: %v, %s", len(candles), "length of candles is not match.")
@@ -70,9 +60,6 @@ func TestCreateOHLC10Sec(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	for i, c := range candles {
-		fmt.Printf("%v, %v\n", i, c)
-	}
 	if len(candles) != 2 {
 		t.Fatalf("Expect: 2, Actual: %v, %s", len(candles), "length of candles is not match.")
 	}
@@ -89,14 +76,14 @@ func getExecutions() []Execution {
 	execDate7, _ := time.Parse(time.RFC3339Nano, "2019-03-01T00:00:10.0Z")
 	execDate8, _ := time.Parse(time.RFC3339Nano, "2019-03-01T00:00:10.999Z")
 
-	e1 := Execution{Id: 1, ExecDate: execDate1, Price: 100, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-1", SellChildOrderAcceptanceId: "sell-1", ReceivedTime: execDate1}
-	e2 := Execution{Id: 2, ExecDate: execDate2, Price: 102, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate2}
-	e3 := Execution{Id: 3, ExecDate: execDate3, Price: 99, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate3}
-	e4 := Execution{Id: 4, ExecDate: execDate4, Price: 101, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate4}
-	e5 := Execution{Id: 5, ExecDate: execDate5, Price: 110, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate5}
-	e6 := Execution{Id: 6, ExecDate: execDate6, Price: 120, Size: 0.01, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate6}
-	e7 := Execution{Id: 7, ExecDate: execDate7, Price: 200, Size: 0.01, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate7}
-	e8 := Execution{Id: 8, ExecDate: execDate8, Price: 300, Size: 1.01, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate8}
+	e1 := Execution{Id: 1, ExecDate: execDate1, Price: 100, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-1", SellChildOrderAcceptanceId: "sell-1", ReceivedTime: execDate1.Add(100 * time.Millisecond)}
+	e2 := Execution{Id: 2, ExecDate: execDate2, Price: 102, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate2.Add(200 * time.Millisecond)}
+	e3 := Execution{Id: 3, ExecDate: execDate3, Price: 99, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate3.Add(300 * time.Millisecond)}
+	e4 := Execution{Id: 4, ExecDate: execDate4, Price: 101, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate4.Add(400 * time.Millisecond)}
+	e5 := Execution{Id: 5, ExecDate: execDate5, Price: 110, Size: 0.1, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate5.Add(500 * time.Millisecond)}
+	e6 := Execution{Id: 6, ExecDate: execDate6, Price: 120, Size: 0.01, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate6.Add(600 * time.Millisecond)}
+	e7 := Execution{Id: 7, ExecDate: execDate7, Price: 200, Size: 0.01, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate7.Add(1500 * time.Millisecond)}
+	e8 := Execution{Id: 8, ExecDate: execDate8, Price: 300, Size: 1.01, Side: "BUY", BuyChildOrderAcceptanceId: "buy-2", SellChildOrderAcceptanceId: "sell-2", ReceivedTime: execDate8.Add(500 * time.Millisecond)}
 	executions := []Execution{e1, e2, e3, e4, e5, e6, e7, e8}
 	return executions
 }
--- restapi_test.go
+++ restapi_test.go
@@ -11,38 +11,30 @@ const productCode = "FX_BTC_JPY"
 var api *Bitflyer
 
 func TestMain(m *testing.M) {
-	println("before all...")
-
 	apiKey := os.Getenv("APIKEY")
 	apiSecret := os.Getenv("APISECRET")
 	code := 1
 	if apiKey != "" && apiSecret != "" {
-		fmt.Println("apiKey:", apiKey)
-		fmt.Println("apiSecret:", apiSecret)
 		api = NewBitflyer(apiKey, apiSecret, []int{-1}, 1, 1)
 		code = m.Run()
 	} else {
 		fmt.Println("Environment variables must be defined. [APIKEY=<api key>, APISECRET=<api secret>]")
 	}
-
-	println("after all...")
 	os.Exit(code)
 }
 
 func TestGetPositions(t *testing.T) {
-	positions, err := api.GetPositions(productCode)
+	_, err := api.GetPositions(productCode)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("positions:", positions)
 }
 
 func TestGetCollateral(t *testing.T) {
-	collateral, err := api.GetCollateral()
+	_, err := api.GetCollateral()
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("collateral:", collateral)
 }
 
 //func TestGetMeExecutions(t *testing.T) {
@@ -63,9 +55,8 @@ func TestGetMeChildOrders(t *testing.T) {
 		"product_code": productCode,
 		"count":        "1",
 	}
-	childOrders, err := api.GetChildOrders(params)
+	_, err := api.GetChildOrders(params)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("childOrders:", childOrders)
 }
